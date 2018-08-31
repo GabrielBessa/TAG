fd7732166bf2c11507a9ad44de14b088c94ae758
GabrielBessa <gabrielbessarofl@gmail.com>
2018-08-31 18:54:35 -0300

    Added description needed to the main file

--- Trabalho1/t1.cpp
+++ Trabalho1/t1.cpp
@@ -1,4 +1,23 @@
-#include <iostream>
-#include <vector>
-#include <map>
-#include <string>
+// Study case of W. W. Zachary's Karate Club (1977)
+// For UnB - Theory and Application of Graphs, 2018/2
+// by Gabriel Bessa 16/0120811 and Andre Cassio  16/0xxxxxx
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
